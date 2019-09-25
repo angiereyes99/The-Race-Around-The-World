4461134e638cb55d917f8ce00acfc1c7b083ada6
Angelo Reyes <48266187+angiereyes99@users.noreply.github.com>
2019-09-25 15:53:04 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//this is where to start the app
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended:true }));
+app.set('view engine', 'ejs');
+app.get('/', function(req,res){
+    res.render('index');
+});
+app.listen(3000,function(){
+    console.log("Connected on port 3000!");
+})
