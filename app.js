50872a3e7c187cb9b521f4277ee8ebbd8ee7f213
Angelo Reyes <angeloreyes@Angelos-MacBook-Air.local>
2019-09-25 23:26:54 -0700

    basic styles/need to create funcs

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('views'));
 app.use(bodyParser.urlencoded({ extended:true }));
 app.set('view engine', 'ejs');
 app.get('/', function(req,res){
     res.render('index');
 });
-app.listen(3000,function(){
-    console.log("Connected on port 3000!");
+app.listen(8080,function(){
+    console.log("Connected on port 8080!");
 })
---
+++ views/scripts/watchTime.js
@@ -0,0 +1,5 @@
+//this will be used to set the timer going for the stop watch
+var localStorage = window.localStorage;
+var data = localStorage.getItem('watchData');
+var start = undefined;
+var stop  = undefined;
